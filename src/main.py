f7f5c62f87c6210b436beab1547afa05cb665b13
Marvin <muellermarvin@gmail.com>
2023-04-25 18:45:59 +0200

    Small refactorings and a sleep for login, if its too fast the login will fail

--- src/main.py
+++ src/main.py
@@ -1,34 +1,29 @@
 import os
 
 import requests
+import time
 
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
-params = {
-    'mode': 'login',
-}
-
 
 def main():
-    login_page = requests.get(f'{os.getenv("BASEURL")}/ucp.php', params=params).text
+    login_page = requests.get(f'{os.getenv("PHPBB_BASEURL")}/ucp.php?mode=login').text
     soup = BeautifulSoup(login_page)
-    sid_raw = soup.find('input', {'name': 'redirect'}).attrs['value']
-    sid = sid_raw.replace('./ucp.php?mode=login&sid=', '')
+    sid = soup.find('input', {'name': 'redirect'}).attrs['value'].replace('./ucp.php?mode=login&sid=', '')
     creation_time = soup.find('input', {'name': 'creation_time'}).attrs['value']
     form_token = soup.find('input', {'name': 'form_token'}).attrs['value']
 
     data = {
-        'username': os.getenv('USERNAME'),
-        'password': os.getenv('PASSWORD'),
+        'username': os.getenv('PHPBB_USERNAME'),
+        'password': os.getenv('PHPBB_PASSWORD'),
         'creation_time': creation_time,
         'form_token': form_token,
         'sid': sid,
         'login': 'Anmelden',
     }
-
-    response = requests.post(f'http{os.getenv("BASEURL")}/ucp.php', params=params, data=data)
-    # response will show you are logfged in!
+    time.sleep(1)
+    response = requests.post(f'{os.getenv("PHPBB_BASEURL")}/ucp.php?mode=login', data=data)
 
 
 if __name__ == '__main__':
